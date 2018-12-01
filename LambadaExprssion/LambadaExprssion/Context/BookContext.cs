98bc43cbe3c48c38fa40eddb99407478b132b3a6
thuonghvd0056 <thuonghvd00564@fpt.edu.vn>
2018-12-01 21:11:25 +0700

    sem 3 lambada

---
+++ LambadaExprssion/LambadaExprssion/Context/BookContext.cs
@@ -0,0 +1,15 @@
+﻿using LambadaExprssion.Models;
+using Microsoft.Ajax.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace LambadaExprssion.Context
+{
+    public class BookContext : DbContext
+    {
+        public DbSet<Book> Books { get; set; }
+    }
+}
